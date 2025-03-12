96ded332834adcbc23c423d80eff0b78975c60c6
Mateus Macedo <mateusmacedobatista05@gmail.com>
2025-03-12 14:56:16 -0300

    estilização e criação de listas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
 import Logo from './components/Logo'
+import perfil from './images/perfil.svg'
+import sacola from './images/sacola.svg'
+
+const textoOpcoes = ['Catregorias', 'Favoritos', 'Minha estante']
+const icons = [perfil, sacola]
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <Logo></Logo>
+        <ul className='options'>
+          {textoOpcoes.map( (texto) => (
+            <li className='option'><p>{texto}</p></li>
+          ) )}
+        </ul>
+        
+        <ul className='icones'>
+            {icons.map( (icons) => (
+              <li className='icone'><img src={icons}></img> </li>
+            ) ) }
+        </ul>
       </header>
     </div>
   );
--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -3,7 +3,9 @@ import './style.css'
 function Logo() {
     return (
         <div className='logo'>
-            <img src={logo} alt='Logo'></img>
+            <img src={logo}
+             alt='Logo' 
+             className='logo-img'></img>
             <p><strong>Alura</strong>Books</p>
         </div>
     )
