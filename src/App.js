5e79a0339029cf75e5fe1da6e318ffa6e8e98878
Mateus Macedo <mateusmacedobatista05@gmail.com>
2025-03-24 17:35:35 -0300

    criação do card com props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import Pesquisa from './components/Pesquisa'
+import UltimosLancamentos from './components/UltimosLancamentos'
 import styled from 'styled-components'
 
 const AppContainer = styled.div`
@@ -12,6 +13,7 @@ function App() {
     <AppContainer>
       <Header/>
       <Pesquisa/>
+      <UltimosLancamentos/>
     </AppContainer>
   );
 }
