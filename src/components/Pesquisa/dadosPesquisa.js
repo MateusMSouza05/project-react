c2ae47ecfbc4ad71ed08e567ab2c7d2c4b13bfb9
Mateus Macedo <mateusmacedobatista05@gmail.com>
2025-03-24 16:10:36 -0300

    criação da parte de pesquisa

---
+++ src/components/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,8 @@
+import livro from '../../images/livro.png'
+
+export const livros = [
+    {nome: 'Liderança em Design', id: 1, src:livro},
+    {nome: 'JavaScript assertivo', id: 2, src:livro},
+    {nome: 'C# do zero', id: 3, src:livro},
+    {nome: 'metodologias ágeis', id: 4, src:livro}
+]
